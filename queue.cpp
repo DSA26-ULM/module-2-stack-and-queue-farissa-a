d23b520fe49a652d223cc32e7047f69125285c4d
farissa-a <fharnoto@gmail.com>
2026-04-21 14:52:56 +0800

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -1,58 +1,68 @@
 #include "queue.h"
 #include <stdexcept>
 
-void init(Queue* q) {
-    q->front = nullptr;
-    q->rear = nullptr;
+void init(Queue* q){
+    q->front = q->data;
+    q->rear = q->data - 1;
 }
 
-bool isEmpty(const Queue* q) {
-    return q->front == nullptr;
+bool isEmpty(const Queue* q){
+    return q->front == q->data && q->rear == q->data - 1;
 }
 
-bool isFull(const Queue* q) {
-    if (q->rear == nullptr) return false;
-    return (q->rear - q->data) >= MAX - 1;
-}
-
-void enqueue(Queue* q, int value) {
-    if (isFull(q)) {
-        throw std::runtime_error("Queue Full");
-    }
+bool isFull(const Queue* q){
+    if (isEmpty(q)) return false;
     
-    if (isEmpty(q)) {
+    const int* nextRear;
+
+    if (q->rear == q->data + MAX - 1)
+        nextRear = q->data;
+    else
+        nextRear = q->rear + 1;
+    return nextRear == q->front;
+}
+
+void enqueue(Queue* q, int value){
+    if (isFull(q))
+        throw std::overflow_error("Queue penuh!");
+
+    if (isEmpty(q)){
         q->front = q->data;
         q->rear = q->data;
     } else {
-        q->rear++;
+        if (q->rear == q->data + MAX - 1)
+            q->rear = q->data;
+        else
+            q->rear++;
     }
     *(q->rear) = value;
 }
 
 void dequeue(Queue* q) {
-    if (isEmpty(q)) {
-        throw std::runtime_error("Queue Empty");
-    }
-    
-    if (q->front == q->rear) {
-        q->front = nullptr;
-        q->rear = nullptr;
+    if (isEmpty(q))
+        throw std::underflow_error("Queue kosong!");
+
+    if (q->front == q->rear){
+        q->front = q->data;
+        q->rear = q->data - 1;
     } else {
-        q->front++;
+        if (q->front == q->data + MAX - 1)
+            q->front = q->data;
+        else
+            q->front++;
     }
 }
 
-int front(const Queue* q) {
-    if (isEmpty(q)) {
-        throw std::runtime_error("Queue Empty");
-    }
+int front(const Queue* q){
+    if (isEmpty(q))
+        throw std::underflow_error("Queue kosong!");
+
     return *(q->front);
 }
 
-// Mengambil nilai paling belakang (sering diminta oleh test_queue)
-int back(const Queue* q) {
-    if (isEmpty(q)) {
-        throw std::runtime_error("Queue Empty");
-    }
+int back(const Queue* q){
+    if (isEmpty(q))
+        throw std::underflow_error("Queue kosong!");
+
     return *(q->rear);
 }
